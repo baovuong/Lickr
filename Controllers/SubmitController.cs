531ef3177abe79c34542a2265b667b6b07fd2ffc
Bao Vuong <vqbao@lv5.org>
2018-01-30 12:06:57 -0500

    moved submission page to its own controller. add another l i c k

---
+++ Controllers/SubmitController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lickr.Models;
+using Lickr.Dispensers;
+
+namespace Lickr.Controllers
+{
+    public class SubmitController : Controller
+    {
+        private readonly ISongDispenser _songDispenser;
+
+        public SubmitController(ISongDispenser songDispenser)
+        {
+            _songDispenser = songDispenser;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
