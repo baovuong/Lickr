8dc85356ced557ef88f0cb40287753c877f7449e
Bao Vuong <vqbao@lv5.org>
2018-02-09 19:28:52 -0500

    fixed up the model

--- Models/SongSubmissionViewModel.cs
+++ Models/SongSubmissionViewModel.cs
@@ -1,18 +1,28 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Lickr.Models
 {
     public class SongSubmissionViewModel
     {
         [Required]
-        [Display]
+        [Display(Name = "Source/URL")]
         public string Source { get; set; }
 
         [Required]
+        [Display(Name = "Start Timestamp")]
         public TimeSpan Start { get; set; }
 
         [Required]
+        [Display(Name = "End Timestamp")]
         public TimeSpan End { get; set; }
+
+        [Required]
+        [Display(Name = "Fuck")]
+        public SourceType Type { get; set; }
+
+        public IEnumerable<SelectListItem> SourceTypeItems { get; set; }
     }
 }
